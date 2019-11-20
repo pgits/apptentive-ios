d17359439ac381465a675e073ed839627bf5d73d
Peter M. Gits <pgits@geekgaps.com>
2019-11-20 14:38:33 -0600

    Changes to be committed: 	modified:   Package.swift
    added path for source to target and tests

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "apptentive-ios",
-            dependencies: []),
+            path: "Apptentive"),
+            //dependencies: []),
         .testTarget(
             name: "apptentive-iosTests",
-            dependencies: ["apptentive-ios"]),
+            dependencies: ["apptentive-ios"],
+            path: "Apptentive"),
     ]
 )
