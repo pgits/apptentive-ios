e479bbc32024eef3ed88df38c32ea85c0c278aaf
Peter M. Gits <pgits@geekgaps.com>
2019-11-20 14:50:41 -0600

    modified:   Package.swift made specific changes to add the Apptentive/Apptentive path for target and then for tests to Apptentive/ApptentiveTests

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "apptentive-ios",
-            path: "Apptentive"),
+            path: "Apptentive/Apptentive"),
             //dependencies: []),
         .testTarget(
             name: "apptentive-iosTests",
             dependencies: ["apptentive-ios"],
-            path: "Apptentive"),
+            path: "Apptentive/ApptentiveTests"),
     ]
 )
