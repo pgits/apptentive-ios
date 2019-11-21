69dfb65ab302f341c750304435bb041113227f2e
Peter M. Gits <pgits@geekgaps.com>
2019-11-21 16:18:18 -0600

    Changes to be committed: 	modified:   Package.swift removed the testing target cause I couldn't get it to work.

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "apptentive-ios",
+            dependencies: [],
             path: "Apptentive/Apptentive"),
-            //dependencies: []),
-        .testTarget(
-            name: "apptentive-iosTests",
-            dependencies: ["apptentive-ios"],
-            path: "Apptentive/ApptentiveTests"),
+//        .testTarget(
+//            name: "apptentive-iosTests",
+//            dependencies: ["apptentive-ios"],
+//            path: "Apptentive/ApptentiveTests"),
     ]
 )
